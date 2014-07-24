4987776759c72ae0d4ef5f2309fa9cdb427914a6
David Becerra <david.becerra@me.com>
2014-07-24 10:30:52 -0700

    add dummy verification form for testing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations',
                                     confirmations: 'confirmations' }
 
+  get 'user/:id', to: 'verifications#create', as: :user
   get '/unverified', to: 'pages#unverified'
 
   root 'pages#index'
