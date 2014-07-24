84435612b3be8185637a73c3f97c9d7092fd8726
David Becerra <david.becerra@me.com>
2014-07-23 18:16:56 -0700

    add viewing the sign up form spec

--- spec/feature/user_signup_spec.rb
+++ spec/feature/user_signup_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
 feature 'Allows users to signup' do
-  scenario 'Viewing the signup page' do
+  scenario 'Viewing signup/signin options on homepage' do
     visit root_url
 
-    expect(page).to have_content('Signup')
-    expect(page).to have_content('Signin')
+    expect(page).to have_link('Sign up', href: 'users/sign_up')
+    expect(page).to have_link('Sign in', href: 'users/sign_in')
+  end
+
+  scenario 'Viewing the signup form' do
+    visit root_url
+    click_link 'Sign up'
+
+    expect(page).to have_selector('form#new_user')
   end
 end
