3cad2b99c30bb3ed5d802479d892171ddab73ee7
David Becerra <david.becerra@me.com>
2014-07-23 20:04:14 -0700

    attach devise to user model

--- spec/feature/user_signup_spec.rb
+++ spec/feature/user_signup_spec.rb
@@ -14,4 +14,19 @@ feature 'Allows users to signup' do
 
     expect(page).to have_selector('form#new_user')
   end
+
+  scenario 'User signs up' do
+    visit root_url
+    click_link 'Sign up'
+    within('form#new_user') do
+      fill_in 'First Name', with: 'Bruce'
+      fill_in 'Middle Name', with: 'Thomas'
+      fill_in 'Last Name', with: 'Wayne'
+      fill_in 'Date of Birth', with: '2-19-1939'
+      fill_in 'Email', with: 'bruce@example.com'
+      fill_in 'Password', with: 'thebat4eva'
+      fill_in 'Password Confirmation', with: 'thebat4eva'
+      click_button 'Sign up'
+    end
+  end
 end
