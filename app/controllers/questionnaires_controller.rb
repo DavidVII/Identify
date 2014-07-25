da1bd6cdd475b7046dc953c593a9b1a467680f03
David Becerra <david.becerra@me.com>
2014-07-25 15:02:38 -0700

    check user before loading the questionnaire view

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -1,7 +1,18 @@
 class QuestionnairesController < ApplicationController
+  before_action :check_user
+
   def show
     @user = current_user
     @questions_hash = current_user.retrieve_questions
+  end
+
+  private
+
+  def check_user
+    unless current_user.verified?
+      flash[:alert] = "Whoa cowboy! You'll need to be verified first."
+      redirect_to edit_address_path(current_user.address)
+    end
   end
 end
 
